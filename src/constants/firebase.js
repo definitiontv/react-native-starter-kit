da6c77cdb44e936ccca52864ca37805eef9d5154
James <johnbeck@definition.tv>
2018-01-02 16:15:22 +0000

    firebase links

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: 'AIzaSyAHshJehJ-E_9AoUN3kfY8BLvZtFoXxFto',
-  authDomain: 'react-native-starter-app.firebaseapp.com',
-  databaseURL: 'https://react-native-starter-app.firebaseio.com',
-  projectId: 'react-native-starter-app',
-  storageBucket: 'react-native-starter-app.appspot.com',
-  messagingSenderId: '762822550667',
+  apiKey: "AIzaSyBcZbyAMEZk4EklR3lMGK-vFPLiD4EfzQo",
+    authDomain: "mobility-55664.firebaseapp.com",
+    databaseURL: "https://mobility-55664.firebaseio.com",
+    projectId: "mobility-55664",
+    storageBucket: "mobility-55664.appspot.com",
+    messagingSenderId: "917523552645",
 };
